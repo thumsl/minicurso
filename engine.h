3cde95a04d68784788f6289bb47ab796f36c3aca
Luan Cristian Thums <luancthums@gmail.com>
2016-08-29 09:18:27 -0300

    engine.h

---
+++ engine.h
@@ -0,0 +1,9 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "window.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#endif // ENGINE_H
--- game.c
+++ game.c
@@ -1,4 +1,12 @@
+
+#include "engine.h"
+
 int main() {
+	SDL_Window *window = window_create("3D Game Engine", WIDTH, HEIGHT);
+
+	SDL_GL_SwapWindow(window);
+
+	SDL_Delay(2000);
 
 	return 0;
 }
